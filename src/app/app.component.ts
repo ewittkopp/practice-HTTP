51cd9c558d68450eb7183bbd9bb27fc9a7114b03
Ethan Wittkopp <ethan.wittkopp@ndsu.edu>
2026-04-01 11:52:38 -0500

    Added post retrieval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taskList';
+  dataService = inject(DataService);
+  ngOnInit() {
+    this.dataService.loadPosts();
+  }
 }
-
